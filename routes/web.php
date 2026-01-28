ae43f470cb86e0a0fc3ed6134ce053f63df8b540
awad3317 <127610663+awad3317@users.noreply.github.com>
2026-01-28 21:42:04 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,58 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+/*
+|--------------------------------------------------------------------------
+| 1. Public Web Routes (مجموعة الموقع العام)
+|--------------------------------------------------------------------------
+| هذه الروابط متاحة للجميع (زوار ومستخدمين) ولا تتطلب تسجيل دخول.
+|
+*/
 
-Route::get('/shop', function () {
-    return view('web.shop.index');
-})->name('shop');
+Route::group([], function () {
+    
+    // الصفحة الرئيسية
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/recipes', function () {
-    return view('web.recipes.index');
-})->name('recipes');
+    // قسم المتجر
+    Route::get('/shop', function () {
+        return view('web.shop.index');
+    })->name('shop'); 
 
-Route::get('/recipe-details', function () {
-    return view('web.recipes.show'); // Note: Controller usually handles this, but updating this closure just in case it's used
-})->name('recipe.details');
+    // قسم الوصفات
+    Route::get('/recipes', function () {
+        return view('web.recipes.index');
+    })->name('recipes');
 
-Route::get('/contact', function () {
-    return view('web.contact');
-})->name('contact');
+    Route::get('/recipe-details', function () {
+        return view('web.recipes.show');
+    })->name('recipe.details');
 
-Route::view('/policy', 'web.return-policy')->name('policy');
+    // صفحات المعلومات (تواصل معنا، الشروط، السياسة)
+    Route::get('/contact', function () {
+        return view('web.contact');
+    })->name('contact');
+    
+    Route::view('/policy', 'web.return-policy')->name('policy');
+    Route::view('/terms', 'web.terms')->name('terms');
 
-Route::view('/terms', 'web.terms')->name('terms');
+    // السلة (متاحة للجميع عادة)
+    // Route::get('/cart', function () {
+    //     return view('web.partials.cart-drawer'); // أو web.cart.index حسب ترتيبك
+    // })->name('cart.index');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 2. Dashboard & Authenticated Routes (مجموعة لوحة التحكم)
+|--------------------------------------------------------------------------
+| هذه الروابط تتطلب تسجيل دخول (Auth Middleware).
+| الرابط سيبدأ بـ /dashboard
+| الاسم سيبدأ بـ dashboard.
+|
+*/
+
+Route::middleware([])->prefix('dashboard')->name('dashboard.')->group(function () {
+    //
+});
+
